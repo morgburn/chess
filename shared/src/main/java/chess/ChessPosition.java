d73eea01e98d966b39c50719c6e1d715724e414a
Morgan Burnside <morganburn670@gmail.com>
2026-09-10 20:31:04 -0600

    initializing variables and adding returns

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -8,7 +8,12 @@ package chess;
  */
 public class ChessPosition {
 
+    private final int row;
+    private final int col;
+
     public ChessPosition(int row, int col) {
+        this.row  = row;
+        this.col = col;
     }
 
     /**
@@ -16,14 +21,13 @@ public class ChessPosition {
      * 1 codes for the bottom row
      */
     public int getRow() {
-        throw new RuntimeException("Not implemented");
+        return row;
     }
-
     /**
      * @return which column this position is in
      * 1 codes for the left column
      */
     public int getColumn() {
-        throw new RuntimeException("Not implemented");
+        return col;
     }
 }
