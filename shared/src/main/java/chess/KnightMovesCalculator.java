530aaebccac730265a5e3f64aa0f3561a6aa5f6b
Morgan Burnside <morganburn670@gmail.com>
2026-09-14 21:07:16 -0600

    Add implemenation using comments in KnightMovesCalculator

--- shared/src/main/java/chess/KnightMovesCalculator.java
+++ shared/src/main/java/chess/KnightMovesCalculator.java
@@ -13,36 +13,44 @@ public class KnightMovesCalculator {
         if (row < 7) {
             if (col < 8) {
                 //check the square two squares up and one square right
+                addMoves(board, myPosition, possibleMoves, row+2, col+1);
             }
             if (col > 1) {
                 //check the square two squares up and one square left
+                addMoves(board, myPosition, possibleMoves, row+2, col-1);
             }
         }
 
         if (row > 2) {
             if (col < 8) {
                 //check the square two squares down and one square right
+                addMoves(board, myPosition, possibleMoves, row-2, col+1);
             }
             if (col > 1) {
                 //check the square two squares down and one square left
+                addMoves(board, myPosition, possibleMoves, row-2, col-1);
             }
         }
 
         if (col < 7) {
             if (row < 8) {
                 //check the square two squares right and one square up
+                addMoves(board, myPosition, possibleMoves, row+1, col+2);
             }
             if (row > 1) {
                 //check the square two squares right and one square down
+                addMoves(board, myPosition, possibleMoves, row-1, col+2);
             }
         }
 
         if (col > 2) {
             if (row < 8) {
                 //check the square two squares left and one square up
+                addMoves(board, myPosition, possibleMoves, row+1, col-2);
             }
             if (row > 1) {
                 //check the square two squares left and one square down
+                addMoves(board, myPosition, possibleMoves, row-1, col-2);
             }
         }
 
